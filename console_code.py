a9c6f21351d384a7b5a05ba47263c742b4db1092
Santiago1031 <scordoba067@soyudemedellin.edu.co>
2023-08-19 11:13:39 -0500

    Interfaz de usuario para calcular cuotas mensuales.

--- console_code.py
+++ console_code.py
@@ -1,17 +1,17 @@
-"""
-import pago_con_intereses as pg
 
+import pago_tarjeta_credito as ptc
+
+""""
 La interfaz de usuario del programa debe separarse del modulo 
 que contiene la lógica.
 
-En este caso, la interfaz de usuario queda en CreditCardConsole.py
-y la lógica queda en Payment.py
-
-
-print("Este programa le permite calcular la cuota a pagar por una compra con tarjeta de credito")
-monto = float(input("Monto de la compra:"))
-tasa_intereses = float(input("Tasa de interés de la tarjeta:"))
-cuotas = float(input("Numero de cuotas en que va a diferir la compra:"))
-
-print(pg.calcular_intereses(monto, tasa_intereses, cuotas))
+En este caso, la interfaz de usuario queda en casos_de_prueba_intereses.py
+y la lógica queda en pago_tarjeta_credito.py
 """
+
+print("Este programa le permite calcular la cuota a pagar por una compra con tarjeta de crédito")
+monto = float(input("Monto de la compra: "))
+tasa_intereses = float(input("Tasa de interés de la tarjeta: "))
+cuotas = float(input("Número de cuotas en que va a diferir la compra: "))
+
+print(ptc.calcular_intereses(monto, tasa_intereses, cuotas))
